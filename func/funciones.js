a553f44b20bd6a05fb0a66510e7086891593161c
jperdo02 <54850199+jperdo02@users.noreply.github.com>
2019-09-04 22:47:03 -0300

    Solucion Ejercicio 1 Johan Perdomo

---
+++ func/funciones.js
@@ -0,0 +1,13 @@
+function functionClick(){ 			
+			
+	if (document.getElementById("texto").style.display !== "none"){
+		
+		document.getElementById("texto").style.display="none";
+		
+	}else{
+		
+		document.getElementById("texto").style.display="inherit";
+		
+	}
+			
+}
